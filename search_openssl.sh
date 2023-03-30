00a92a4adf465747cb86ee58c60044804626962d
tomek-skrond <tomek2.skrond@gmail.com>
2023-03-30 19:00:56 -0400

    added comments for readability + argument support

--- search_openssl.sh
+++ search_openssl.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-hostname=google.com
+hostname=$1
 
+if [ $# -eq 0 ];then
+	echo "No arguments passed, defaulting to hostname=google.com"
+	hostname=google.com
+fi
+
+#getting raw output of all openssl supported ciphers
 ciphers_raw=$(openssl ciphers)
+#converting ciphers into an array of cipher names
 ciphers=($(echo $ciphers_raw | tr ':' "\n"))
 
+#displaying all ciphers
 echo ${ciphers[@]}
 
+#iterating through all ciphers
 for c in ${ciphers[@]}; do
 	
 	echo Testing Cipher: $c
@@ -15,8 +24,10 @@ for c in ${ciphers[@]}; do
 	if [[ "$(echo $openssl_output | grep -iwo failed)" == "failed" ]];then
 		echo CIPHER $c NOT COMPATIBLE WITH $hostname
 
+		#if cipher not compatible for connection -> save to csv with '0'
 		echo $c,0 >> ${hostname}_comp.csv
 	else
+		#if cipher compatible -> save to csv with '1'
 		echo OK
 		echo $c,1 >> ${hostname}_comp.csv
 	fi
